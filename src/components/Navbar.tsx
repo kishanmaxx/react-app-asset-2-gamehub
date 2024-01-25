6368aa5f2c489ddabe77bd388ad4cdc922446ad2
Kishan Shetty <kishanmaxx@gmail.com>
2024-01-25 13:46:28 +0530

    Implementing the search functionality.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,14 @@ import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/node-js-icon-1817x2048-g8tzf91e.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
-
-const Navbar = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+const Navbar = ({ onSearch }: Props) => {
   return (
     <HStack justifyContent="space-between" padding="13px">
       <Image src={logo} boxSize="60px" />
-      <SearchInput />
+      <SearchInput onSearch={onSearch} />
       <ColorModeSwitch />
     </HStack>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -16,6 +16,6 @@ export interface Game {
   }
   
 const useGames = (gameQuery:GameQuery)=>useData<Game>('/games',
-{params:{genre:gameQuery.genre?.id,platforms:gameQuery.platform?.id,ordering:gameQuery.sortOrder}},[gameQuery]);
+{params:{genre:gameQuery.genre?.id,platforms:gameQuery.platform?.id,ordering:gameQuery.sortOrder,search:gameQuery.searchText}},[gameQuery]);
 
 export default useGames;
